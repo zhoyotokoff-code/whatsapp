bbb15b1e3530ae23567846384ec190875fdb219c
zhoyotokoff-code <zhoyotokoff@gmail.com>
2026-05-31 23:41:14 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -93,15 +93,7 @@ const nextConfig: NextConfig = {
    */
   async headers() {
     return [
-      {
-        source: "/_next/static/:path*",
-        headers: [
-          {
-            key: "Cache-Control",
-            value: "public, max-age=31536000, immutable",
-          },
-        ],
-      },
+
       {
         source: "/api/:path*",
         headers: [{ key: "Cache-Control", value: "no-store" }],
